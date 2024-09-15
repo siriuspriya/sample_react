aa5835a900c81406c71fe3dbc93e44a720a65362
devopsprasanthi <147678076+devopsprasanthi@users.noreply.github.com>
2024-09-14 21:32:46 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Make API call to Node.js backend
+    axios.get('http://localhost:5000/api/hello')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello World from React!</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
